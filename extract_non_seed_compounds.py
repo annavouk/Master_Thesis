89a86ae6e6fefd01b4d7857935ea9033b9352608
annavouk <voukounaa@example.com>
2025-02-14 15:09:05 +0100

    seeds/non_seeds for each patric_id

---
+++ extract_non_seed_compounds.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+def extract_non_seed_compounds(file_path):
+        df = pd.read_pickle(file_path)
+
+        genome_compounds = {}
+
+        for genome_id, row in df.iterrows():
+                compounds = row[row == 1].index.tolist()
+
+                if compounds:
+                        genome_compounds[genome_id] = compounds
+
+        return genome_compounds
+
+file_path = "non_seeds_binary_per_patric.pckl"
+output_file = "non_seed_compounds_for_each_patric_id.csv"
+
+result = extract_non_seed_compounds(file_path)
+
+result_df = pd.DataFrame(list(result.items()), columns=['Genome ID', 'non Seed Compounds'])
+result_df.to_csv(output_file, index=False)
