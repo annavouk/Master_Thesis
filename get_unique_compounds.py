17273d2203e9ab206919a0914f1fc06f3b4b0536
annavouk <voukounaa@example.com>
2025-02-19 13:47:00 +0100

    correct version

--- get_unique_compounds.py
+++ get_unique_compounds.py
@@ -3,13 +3,14 @@ import pandas as pd
 df1 = pd.read_pickle("seeds_binary_per_patric.pckl")
 df2 = pd.read_pickle("non_seeds_binary_per_patric.pckl")
 
-compound_columns_df1 = [col for col in df1.columns if col.startswith('cpd')]
-compound_columns_df2 = [col for col in df2.columns if col.startswith('cpd')]
+compound_columns_1 = [col for col in df1.columns if col.startswith('cpd')]
+compound_columns_2 = [col for col in df2.columns if col.startswith('cpd')]
 
-compounds_df = pd.concat([pd.Series(compound_columns_df1), pd.Series(compound_columns_df2)], ignore_index=True)
-compounds_df = compounds_df.drop_duplicates()
+compounds = pd.concat([pd.Series(compound_columns_1), pd.Series(compound_columns_2)], ignore_index=True)
+compounds = compounds.drop_duplicates()
+compounds = compounds.sort_values()
 
-compounds_df.to_csv("unique_compounds.csv", index=False, header=False)
+compounds.to_csv("unique_compounds.csv", index=False, header=['Compound'])
 
-print(compounds_df.head())
-print(compounds_df.tail())
+print(compounds.head())
+print(compounds.tail())
