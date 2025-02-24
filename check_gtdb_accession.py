f2c3f5c4ceb5887954516b889904e94e8d16bc09
annavouk <voukounaa@example.com>
2025-02-24 20:54:50 +0100

    Check if all PATRIC IDs are present in GTDB metadata

--- check_gtdb_accession.py
+++ check_gtdb_accession.py
@@ -1,18 +1,21 @@
 import pandas as pd
 
 patric_file = "patric_ids_with_gtdb_taxonomy.csv"
-gtdb_file = "gtdb_metadata_r207.tsv"
+gtdb_file = "gtdb_accession_taxonomy.csv"
+output_file = "missing_accessions.txt"
 
-patric_df = pd.read_csv(patric_file)
-gtdb_df = pd.read_csv(gtdb_file, sep="\t")
+patric_df = pd.read_csv(patric_file, low_memory=False)
+gtdb_df = pd.read_csv(gtdb_file, low_memory=False)
 
 patric_assembly_accessions = set(patric_df["assembly_accession"])
-gtdb_assembly_accessions = set(gtdb_df["accession"])
+gtdb_assembly_accessions = set(gtdb_df["assembly_accession"])
 
 missing_accessions = patric_assembly_accessions - gtdb_assembly_accessions
 
 if not missing_accessions:
     print("All assembly_accessions from patric_ids_with_gtdb_taxonomy.csv exist in gtdb_metadata_r207.tsv.")
 else:
-    print(f" {len(missing_accessions)} assembly_accessions are missing:")
-    print("\n".join(missing_accessions))
+    print(f"{len(missing_accessions)} assembly_accessions are missing. Saved to {output_file}")
+
+    with open(output_file, "w") as f:
+        f.write("\n".join(missing_accessions))
