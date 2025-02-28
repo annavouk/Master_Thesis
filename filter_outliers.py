51567c95d04eb04592f584ddfa2440f6f5ac3998
annavouk <voukounaa@example.com>
2025-02-28 15:48:47 +0100

    Filter outliers

--- filter_outliers.py
+++ filter_outliers.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
 df1 = pd.read_pickle('seeds_to_non_seeds_df.pkl')
-metadata = pd.read_csv("merged_metadata.csv", low_memory=False) # patric_id is the first column
+data = pd.read_csv("patric_ids_with_gtdb_taxonomy_correct.csv", low_memory=False) # patric_id is the first column
 
+# Filter groups 0-5% and 95-100% of distribution
 df1_sorted = df1.sort_values(by="Total_Seeds")
 Q1 = df1_sorted["Total_Seeds"].quantile(0.05)
 Q2 = df1_sorted["Total_Seeds"].quantile(0.95)
@@ -21,33 +22,38 @@ low_outliers_reset.rename(columns={low_outliers_reset.columns[0]: 'patric_id'}, 
 high_outliers_reset = high_outliers.reset_index()
 high_outliers_reset.rename(columns={high_outliers_reset.columns[0]: 'patric_id'}, inplace=True)
 
-# Convert 'patric_id' to string (object type) in both DataFrames
-low_outliers_reset['patric_id'] = low_outliers_reset['patric_id'].astype(str)
-metadata['patric_id'] = metadata['patric_id'].astype(str)
+# Convert 'patric_id' to string (object type) in both DataFrames to overcome missing 0s in IDs
+low_outliers_reset['patric_id'] = low_outliers_reset['patric_id'].str.strip()
+high_outliers_reset['patric_id'] = high_outliers_reset['patric_id'].str.strip()
 
-low_outliers_metadata = pd.merge(low_outliers_reset, metadata, on='patric_id', how='left')
+data['patric_id'] = data['patric_id'].astype(str).str.strip()
 
-low_outliers_metadata.to_csv('low_outliers_metadata.csv', index=False)
+low_outliers_gtdb_taxonomy = pd.merge(low_outliers_reset, data, on='patric_id', how='left')
 
-high_outliers_reset['patric_id'] = high_outliers_reset['patric_id'].astype(str)
-metadata['patric_id'] = metadata['patric_id'].astype(str)
+low_outliers_gtdb_taxonomy = low_outliers_gtdb_taxonomy.drop_duplicates(subset=['patric_id'], keep='first')
 
-high_outliers_metadata = pd.merge(high_outliers_reset, metadata, on='patric_id', how='left')
+low_outliers_gtdb_taxonomy.to_csv('low_outliers_gtdb_taxonomy.csv', index=False)
 
-high_outliers_metadata.to_csv('high_outliers_metadata.csv', index=False)
+high_outliers_gtdb_taxonomy = pd.merge(high_outliers_reset, data, on='patric_id', how='left')
+
+high_outliers_gtdb_taxonomy = high_outliers_gtdb_taxonomy.drop_duplicates(subset=['patric_id'], keep='first')
+
+high_outliers_gtdb_taxonomy.to_csv('high_outliers_gtdb_taxonomy.csv', index=False)
 
 # Check if all patric_ids of interest are present in the merged file
-present_ids_low = low_outliers_metadata['patric_id'].isin(low_outliers_list)
+present_ids_low = low_outliers_gtdb_taxonomy['patric_id'].isin(low_outliers_list)
 
 for patric_id, is_present in zip(low_outliers_list, present_ids_low):
 	if not is_present:
-		print(f"{patric_id} is NOT present in the metadata file.")
+		print(f"{patric_id} is NOT present in the merged file.")
 
-present_ids_high = high_outliers_metadata['patric_id'].isin(high_outliers_list)
+present_ids_high = high_outliers_gtdb_taxonomy['patric_id'].isin(high_outliers_list)
 
-for patric_id, is_present in zip(low_outliers_list, present_ids_high):
+for patric_id, is_present in zip(high_outliers_list, present_ids_high):
         if not is_present:
-                print(f"{patric_id} is NOT present in the metadata file.")
+                print(f"{patric_id} is NOT present in the merged file.")
 
-print(high_outliers_metadata)
-print(low_outliers_metadata)
+print(high_outliers_gtdb_taxonomy)
+print(high_outliers)
+print(low_outliers_gtdb_taxonomy)
+print(low_outliers)
