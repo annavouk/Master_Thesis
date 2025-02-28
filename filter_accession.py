f70132aa2c4d799ec0da4f7709f4bbbccbfe2a50
annavouk <voukounaa@example.com>
2025-02-28 14:38:25 +0100

    PATRIC IDs with GTDB taxonomy and assembly accession

--- filter_accession.py
+++ filter_accession.py
@@ -1,19 +1,12 @@
 import pandas as pd
-import numpy as np
 
 patric_df = pd.read_csv('patric_to_assembly.csv')
 
 output_df = pd.DataFrame(columns=['patric_id', 'refseq_accession', 'ncbi_genbank_accession'])
 
 def fill_accession_type(row):
-	refseq = np.nan
-	genbank = np.nan
-
-	if row['assembly_accession'].startswith('GCA_'):
-		refseq = row['assembly_accession']
-	elif row['assembly_accession'].startswith('GCF_'):
-		genbank = row['assembly_accession']
-
+	refseq = row['assembly_accession']
+	genbank = row['assembly_accession']
 	return pd.Series([row['patric_id'], refseq, genbank])
 
 output_df = patric_df.apply(fill_accession_type, axis=1)
