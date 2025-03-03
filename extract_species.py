ab16e7b43f711b41808a728f8418a68d7ae1027d
annavouk <voukounaa@example.com>
2025-03-03 15:16:39 +0100

    Biodiversity study and add genome size to GTDB metadata

--- extract_species.py
+++ extract_species.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#df = pd.read_csv('low_outliers_metadata.csv', low_memory=False)
-df = pd.read_csv('high_outliers_metadata.csv', low_memory=False)
+#df = pd.read_csv("low_outliers_gtdb_taxonomy.csv", low_memory = False)
+df = pd.read_csv("high_outliers_gtdb_taxonomy.csv", low_memory = False)
 
 def extract_species(df):
     species = {
@@ -18,22 +18,7 @@ species_list = extract_species(df)
 
 species_df = pd.DataFrame(species_list, columns=['Species'])
 
-print(species_df)
-
 species_counts = species_df['Species'].value_counts()
 
-# Plotting the bar chart
-plt.figure(figsize=(12, 6))
-species_counts.plot(kind='bar', color='skyblue')
-plt.title('Species Distribution')
-plt.xlabel('Species')
-plt.ylabel('Frequency')
-plt.xticks(rotation=90)  # Rotate the x-axis labels if they are long
-plt.show()
-
-# Plotting the pie chart
-plt.figure(figsize=(8, 8))
-species_counts.plot(kind='pie', autopct='%1.1f%%', startangle=90, cmap='Set3', legend=False)
-plt.title('Species Distribution')
-plt.ylabel('')  # Removing the y-axis label
-plt.show()
+total_species = species_df['Species'].nunique()
+print(f"Total number of unique species: {total_species}")
--- gtdb_metadata_manipulation.py
+++ gtdb_metadata_manipulation.py
@@ -2,13 +2,13 @@ import pandas as pd
 
 df = pd.read_csv('gtdb_metadata_r207.tsv', sep='\t', low_memory=False)
 
-accession_gtdb_taxonomy = df.iloc[:, [0, 16, 54]]
+accession_gtdb_taxonomy = df.iloc[:, [0, 13, 16, 54]]
 
-accession_gtdb_taxonomy.columns = ['accession', 'gtdb_taxonomy', 'ncbi_genbank_assembly_accession']
+accession_gtdb_taxonomy.columns = ['accession', 'genome_size', 'gtdb_taxonomy', 'ncbi_genbank_assembly_accession']
 
 accession_gtdb_taxonomy.loc[:, 'accession'] = accession_gtdb_taxonomy['accession'].str.replace('GB_|RS_', '', regex=True)
 
-accession_gtdb_taxonomy = accession_gtdb_taxonomy.rename(columns={'accession': "refseq_accession", 'gtdb_taxonomy': "gtdb_taxonomy", 'ncbi_genbank_assembly_accession': "ncbi_genbank_accession"})
+accession_gtdb_taxonomy = accession_gtdb_taxonomy.rename(columns={'accession': "refseq_accession", 'genome_size': "genome_size", 'gtdb_taxonomy': "gtdb_taxonomy", 'ncbi_genbank_assembly_accession': "ncbi_genbank_accession"})
 
 print(accession_gtdb_taxonomy.head())
 
